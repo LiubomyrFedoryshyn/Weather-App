7433b91594ff46771b1d608244c8b44b26af81f8
Liubomyr Fedoryshyn <39272268+LiubomyrFedoryshyn@users.noreply.github.com>
2020-11-05 16:43:51 +0200

    Update Conditions.js

--- weather-app/src/components/Conditions/Conditions.js
+++ weather-app/src/components/Conditions/Conditions.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import classes from './Conditions.module.css'
-const conditions = (props) => {
+const conditions = (props) => { // bad naming here, use 'data' or some another word
+// you can use destructuring here 
+// const conditions = ({ responseObj }) => { 
+// it will allow you to manipulate your object manualy, like 'responseObj.cod', not 'props.responseObj.cod' 
    return (
        <div className={classes.Wrapper}>
-           {props.error && <small className={classes.Small}>Please enter a valid city.</small>}
-
-           {props.loading && <div className={classes.Loader} ></div>}
-           {props.responseObj.cod === 200 ?
+      // you can track an error in object which you receive, and show it to a user
+           {props.error && <small className={classes.Small}>Please enter a valid city.</small>} 
+      // separate component
+           {props.loading && <div className={classes.Loader} ></div>}  
+// it would be better if you had moved responseObj properties to some state with useState hook;
+           {props.responseObj.cod === 200 ?// bad practise, would be better if you will check the object length
                <div>
                    <p>City: <strong>{props.responseObj.name}</strong></p>
                    <p>Temperature: <strong>{Math.round(props.responseObj.main.temp)} degrees</strong> </p>
